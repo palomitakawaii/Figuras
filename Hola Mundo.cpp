7f9431cb46646cb6e1992588557a4dbcbe62b5cf
Paloma Escobar <palomitakawaii014@gmail.com>
2020-02-06 19:00:37 -0600

    trabajo pendiente

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -7,11 +7,18 @@ using namespace std;
 		Figura(int _x)
 		{
 			x = _x;
+			cout<<"Hola soy una figura"<<endl;
 		}
 		
 		virtual float perimetro();
 		virtual float area();
 	};
+	class Circulo: public Figura
+	{
+		public:
+			Circulo(int r) : Figura(r) {}
+			}
+	}
 int main(){
 	return 0;
 	
