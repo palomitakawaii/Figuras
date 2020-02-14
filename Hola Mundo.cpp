7731523af197290376665d68ab5608708e01eb77
Paloma Escobar <palomitakawaii014@gmail.com>
2020-02-13 18:28:25 -0600

    loquesea

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -4,22 +4,38 @@ using namespace std;
 	{
 	public:
 		float x;
-		Figura(int _x)
+		Figura(float _x)
 		{
 			x = _x;
 			cout<<"Hola soy una figura"<<endl;
 		}
 		
-		virtual float perimetro();
-		virtual float area();
+		virtual float perimetro() = 0;
+		virtual float area() = 0;
 	};
 	class Circulo: public Figura
 	{
-		public:
-			Circulo(int r) : Figura(r) {}
-			}
-	}
-int main(){
+		private:
+		float r;
+		
+	public:
+		Circulo(float radio) : Figura(radio) 
+		{
+			r = radio;
+			cout<<"Hola soy el hijo y soy un circulo"<<endl;
+		}
+		float perimetro()
+		{
+			return 2 * r * 3.1416;
+		}
+		float area()
+		{
+			return 3.1416 * r * r;
+		}
+	};
+int main()
+{
+	Circulo mi_circulo = Circulo(5);
+	//mi_circulo.area
 	return 0;
-	
 }
